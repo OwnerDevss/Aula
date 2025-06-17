a2763d502f58e76f4634fd861aa92ea3202b0f66
Owner <74803062+OwnerDevss@users.noreply.github.com>
2025-06-17 20:54:22 -0300

    Update correio.js

--- Correio/correio.js
+++ Correio/correio.js
@@ -3,6 +3,10 @@ const mensagens = [
     {
         destinatario: "CORREIO ELEGANTE",
         mensagem: "SUA MENSAGEM AQUI 💌" // Apenas uma mensagem placeholder
+    },
+    {
+        destinatario: "TESTANDO",
+        mensagem: "TESTANDO A SUA MENSAGEM"
     }
 ];
 
